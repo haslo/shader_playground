cef8cf410483fcaeb1db5ccb93f091e58a8cb0ab
Guido Gloor <haslo@haslo.ch>
2023-04-16 01:59:54 +0200

    C# code for clouds done for now

--- Assets/Scripts/CameraClouds.cs
+++ Assets/Scripts/CameraClouds.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel;
+using System;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -24,11 +24,91 @@ public class CameraClouds : MonoBehaviour {
             if (material != null && cloudShader == null) {
                 DestroyImmediate(material);
             }
+
             if (material != null && cloudShader != null && cloudShader != material.shader) {
                 DestroyImmediate(material);
                 material = new Material(cloudShader);
             }
+
             return material;
+        }
+    }
+
+    public void Start() {
+        if (material) {
+            DestroyImmediate(material);
+        }
+    }
+
+    [ImageEffectOpaque]
+    public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (material == null || valueNoiseImage == null) {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        if (camera == null) {
+            camera = GetComponent<Camera>();
+        }
+
+        Material.SetTexture("_ValueNoise", valueNoiseImage);
+        if (sun != null) {
+            Material.SetVector("_SunDir", sun.forward);
+        } else {
+            Material.SetVector("_SunDir", Vector3.up);
+        }
+
+        Material.SetFloat("_MinHeight", minHeight);
+        Material.SetFloat("_MaxHeight", maxHeight);
+        Material.SetFloat("_FadeDist", fadeDist);
+        Material.SetFloat("_Scale", scale);
+        Material.SetFloat("_Steps", steps);
+
+        Material.SetMatrix("_FrustumCornersWS", GetFrustumCorners());
+        Material.SetMatrix("_CameraInvViewMatrix", camera.cameraToWorldMatrix);
+        Material.SetVector("_CameraPosWS", camera.transform.position);
+
+        CustomGraphicsBlit(source, destination, Material, 0);
+    }
+
+    private Matrix4x4 GetFrustumCorners() {
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+        Vector3[] frustumCornerVectors = new Vector3[4];
+
+        camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1),
+            camera.farClipPlane,
+            Camera.MonoOrStereoscopicEye.Mono,
+            frustumCornerVectors);
+        frustumCorners.SetRow(0, frustumCornerVectors[1]);
+        frustumCorners.SetRow(1, frustumCornerVectors[2]);
+        frustumCorners.SetRow(2, frustumCornerVectors[3]);
+        frustumCorners.SetRow(3, frustumCornerVectors[0]);
+
+        return frustumCorners;
+    }
+    
+    static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr) {
+        RenderTexture.active = dest;
+        fxMaterial.SetTexture("_MainTex", source);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        fxMaterial.SetPass(passNr);
+        GL.Begin(GL.QUADS);
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    protected void OnDisable() {
+        if (material) {
+            DestroyImmediate(material);
         }
     }
 }
